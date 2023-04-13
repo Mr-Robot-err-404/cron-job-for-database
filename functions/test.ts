215d6f4c84d2de369edaf929c83cc586d8f8b385
Harold <harrisonzimbo777@gmail.com>
2023-04-13 13:37:26 +0200

    Yoda tries Qui-Gon's green tea

---
+++ functions/test.ts
@@ -0,0 +1,3 @@
+async function test() {
+    console.log("What about the droid attack on the wookies?")
+}
--- index.ts
+++ index.ts
@@ -20,6 +20,3 @@ async function main() {
     await updatePopularVid(gameSubOrder, "gamedev")
     return
 }
-
-main()
-
